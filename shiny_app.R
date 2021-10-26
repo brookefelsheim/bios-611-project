9d1bfbefda477fe69a7b61fafe598bf922f69d39
Brooke Felsheim <brookefelsheim@gmail.com>
2021-10-25 23:02:39 -0400

    Updated shiny app to show CO2 emissions by country using a dropdown menu.

--- shiny_app.R
+++ shiny_app.R
@@ -1,8 +1,25 @@
+library(tidyverse)
 library(shiny)
-ui <- fluidPage("Hello World")
 
-server <- function(input,output,session){
-  # Does nothing
+long_CO2_emissions <- read_csv("derived_data/long_CO2_emissions.csv")
+
+ui <- fluidPage(
+  selectInput("country", "Select a country:",
+              unique(long_CO2_emissions$Country)),
+  plotOutput("emissions_plot")
+)
+
+server <- function(input, output) {
+  output$emissions_plot <- renderPlot(ggplot(long_CO2_emissions %>%
+                                               filter(Country == input$country),
+                                             aes(x = Year, 
+                                                 y = `CO2 Emissions (1000 t)`)) +
+                                        geom_point() + geom_line() + theme_bw() +
+                                        ggtitle(paste(input$country, 
+                                                      "CO2 Emissions by Year")) +
+                                        theme(axis.text=element_text(size=16),
+                                              axis.title=element_text(size=18,face="bold"),
+                                              title=element_text(size=20)))
 }
 
 # Start the Server
