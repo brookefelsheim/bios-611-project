6245b154a11d88f69fac407287d42fd5cafd0ec5
Brooke Felsheim <brookefelsheim@gmail.com>
2021-11-29 01:19:20 +0000

    Plots paired indicator data

---
+++ scripts/plot_paired_indicator_data.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(readxl)
+library(GGally)
+source("scripts/helper_functions.R")
+
+ensure_dir("figures")
+
+all_predictive_data <- read_csv("derived_data/all_predictive_data.csv")
+
+plot <- ggpairs(all_predictive_data[,-1:-2], 
+                columnLabels = c("GHG emissions\n(kg per capita)", 
+                                 "Energy supply\n (GJ per capita)",
+                                 "Renewable energy\n(% of total)",
+                                 "Agricultural area\n(%)", 
+                                 "Forest area\n(%)",
+                                 "Protected area\n(%)", 
+                                 "Economic activity\n(GDP per capita)", 
+                                 "Happiness\n(Survey score)"))
+
+ggsave("figures/paired_indicators.png", 
+       width = 10, height = 10, plot = plot)
